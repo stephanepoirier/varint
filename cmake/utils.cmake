a54f587497adfab744c308f3b89d58effdffc9a2
Stephane Poirier <stephane.poirier01@gmail.com>
2017-01-26 23:16:25 -0500

    Convert CMake project

---
+++ cmake/utils.cmake
@@ -0,0 +1,10 @@
+################################################################################
+#
+# Custom utility functions
+#
+# Author: Stephane Poirier (stephane.poirier01@gmail.com)
+################################################################################
+
+function(status TEXT)
+	message(STATUS "[INFO] ${TEXT}")
+endfunction()
