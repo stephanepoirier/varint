16c6ca8c302b5bfe867bf94caf50a6cb145f4414
Stephane Poirier <stephane.poirier01@gmail.com>
2017-02-07 16:32:14 -0500

    [Tests] Cleanup and migration to GoogleTest

--- test/CompressedSetTests.cpp
+++ test/CompressedSetTests.cpp
@@ -1,75 +1,51 @@
-#include <vector>
+#include "varint/CompressedSet.h"
+
+#include "gtest/gtest.h"
+
 #include <set>
-#include <assert.h>
 #include <iostream>
 
-#include "varint/BasicSet.h"
-#include "varint/CompressedSet.h"
-#include "varint/Set.h"
+void testvec(set<unsigned int>& data) {
+  std::stringstream ss;
 
-using namespace std;
+  CompressedSet myset2;
+  for (auto i : data) {
+    myset2.addDoc(i);
+  }
+  myset2.compact();
+  myset2.write(ss);
 
-bool testvec(set<unsigned int>& data)
-{
-	stringstream ss;
-	{
-      CompressedSet myset2;
-      for (auto i : data)
-	  {
-	    myset2.addDoc(i);
+  CompressedSet myset1;
+  myset1.read(ss);
+
+  ASSERT_TRUE(data.size() == myset1.size());
+  CompressedSet::Iterator it2(&myset1);
+  for (auto idx : data) {
+    ASSERT_TRUE(it2.nextDoc() != NO_MORE_DOCS);
+    ASSERT_TRUE(it2.docID() == idx);
+  }
+  ASSERT_TRUE(it2.nextDoc() == NO_MORE_DOCS);
+}
+
+TEST(CompressedSetTests, GeneralTest) {
+  for (uint32_t b = 0; b <= 28; ++b) {
+    std::cout << "testing1... b = " << b << std::endl;
+    for (size_t length = 128; length < (1U << 12); length += 128) {
+      std::set<unsigned int> data;
+      for (size_t i = 0; i < length; ++i) {
+        unsigned int x = (i + (24 - i) * (12 - i)) % (1U << b);
+        data.insert(x);
       }
-	  // cout << "added " << data.size() << endl;
-	  // myset2.flush();
-      myset2.compact();
-      myset2.write(ss);
-	}
+      testvec(data);
+    }
 
-	CompressedSet myset1;
-	myset1.read(ss);
-
-	assert(data.size() == myset1.size());
-	CompressedSet::Iterator it2(&myset1);
-	for (auto idx : data)
-	{
-		assert(it2.nextDoc()!=NO_MORE_DOCS );
-		assert(it2.docID() == idx);
-	}
-	assert(it2.nextDoc() == NO_MORE_DOCS);
-
-	return true;
-}
-
-void test(){
-
-	for (uint32_t b = 0; b <= 28; ++b) {
-        cout << "testing1... b = " << b << endl;
-        for (size_t length = 128; length < (1U << 12); length += 128) {
-	        //cout << "   length = " << length << endl;
-	        set<unsigned int> data;
-	        for (size_t i = 0; i < length; ++i) {
-                unsigned int x = (i + (24 - i) * (12 - i)) % (1U << b);
-				data.insert(x);
-            }
-			if (!testvec(data)) {
-				return;
-			}
-        }
-        cout << "testing2... b = " << b << endl;
-       	for (size_t length = 1; length < (1U << 9); ++length) {
-	      //  cout << "   length = " << length << endl;
-		    set<unsigned int> data;
-	        for (size_t i = 0; i < length; ++i) {
-	           data.insert((33231 - i + i * i) % (1U << b));
-	        }
-	        if (!testvec(data)) {
-				return;
-			}
-		}
-	}
-	cout << "All test passed succesfully!!" << endl;
-}
-
-
-int main() {
-	test();
+    std::cout << "testing2... b = " << b << std::endl;
+    for (size_t length = 1; length < (1U << 9); ++length) {
+      std::set<unsigned int> data;
+      for (size_t i = 0; i < length; ++i) {
+        data.insert((33231 - i + i * i) % (1U << b));
+      }
+      testvec(data);
+    }
+  }
 }
